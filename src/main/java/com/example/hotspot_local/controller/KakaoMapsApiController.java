a0701e3d48449536d7abad18a4be96d39e6fc047
7SH7 <kimseunghwan7777@gmail.com>
2024-05-06 21:30:49 +0900

    feat: cache를 이용한 음식점 불러오기 1차 완성 -> 차후 프론트측 안 불러와지면, 고쳐야함. (하나만 불러오는 기능은 api만들어야함.

--- src/main/java/com/example/hotspot_local/controller/KakaoMapsApiController.java
+++ src/main/java/com/example/hotspot_local/controller/KakaoMapsApiController.java
@@ -21,19 +21,31 @@ public class KakaoMapsApiController {
 	private final KakaoMapService kakaoMapService;
 
 	@GetMapping("/search")  // 받는 부분을 UserLocationRequest로 받아서 처리하는 코드로 바꿀 것
-	public Mono<ResultOfMaps> searchPlace(/*@RequestParam String query,@RequestParam String category_group_code,*/ @RequestParam double x, @RequestParam double y, @RequestParam int radius) {
+	public Mono<ResultOfMaps> searchPlace(@RequestParam double x, @RequestParam double y, @RequestParam int radius) {
 		String query = "음식점";
 		String category_group_code = "FD6";
 		return kakaoMapService.searchPlace(query, category_group_code, x, y, radius);
 	}
 
-	@GetMapping("/searches")    // the number of food store is 15 stores. <- use this api.
-	public Mono<List<ResultOfMaps>> searchPlaces(/*@RequestParam String query, @RequestParam String category_group_code,*/ @RequestParam double x, @RequestParam double y, @RequestParam int radius) {
-		String query = "음식점";
-		String category_group_code = "FD6";
-		int page = 1;
-		return kakaoMapService.searchPlaces(query, category_group_code, x,y,radius, page);
+	// before find 15 stores (not use cache)
+	@GetMapping("/searches/NoCahce")    // the number of food store is 15 stores. <- use this api.
+	public Mono<List<ResultOfMaps>> searchPlacesNoCache(@RequestParam double x, @RequestParam double y, @RequestParam int radius) {
+	    String query = "음식점";
+	    String category_group_code = "FD6";
+	    int page = 1;
+		kakaoMapService.saveStoresInCache(query, category_group_code, x, y, radius, page);
+		return kakaoMapService.searchPlaces(query, category_group_code, x, y, radius, page);
 	}
+
+
+//	@GetMapping("/searches")    // the number of food store is 15 stores. <- use this api.
+////	public List<ResultOfMaps> searchPlaces(/*@RequestParam String query, @RequestParam String category_group_code,*/ @RequestParam double x, @RequestParam double y, @RequestParam int radius) {
+//	public Mono<List<ResultOfMaps>> searchPlaces(/*@RequestParam String query, @RequestParam String category_group_code,*/ @RequestParam double x, @RequestParam double y, @RequestParam int radius) {
+//		String query = "음식점";
+//		String category_group_code = "FD6";
+//		int page = 1;
+//		return kakaoMapService.saveStoreListInCache(query, category_group_code, x, y, radius, page);
+//	}
 
 	@GetMapping("/searches/threetimes")    // the number of food store is 45 stores.
 	public Mono<List<ResultOfMaps>> searchPlacesTreeTimes(/*@RequestParam String query, @RequestParam String category_group_code,*/ @RequestParam double x, @RequestParam double y, @RequestParam int radius)  {
